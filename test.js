43e859f28cdf3f3858066ad35b4dbe7a4886316a
wayne <wayneborong@gmail.com>
2020-09-02 21:53:34 +1000

    update conflist

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 
 
 
-change s from  clflict branch herethis is actually from bruce branch and concliect happens from master branch and we are going to be confliect in  b branch her  
+and this is conflict
 
 we are going to hacnage hater her e
